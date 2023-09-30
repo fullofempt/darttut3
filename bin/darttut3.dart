b1226152b5ea1e5f8d533ff93ef8e4e40df7d3cf
fullofempt <super.shlykov11@yandex.ru>
2023-09-30 10:38:41 +0500

    end commit

--- bin/darttut3.dart
+++ bin/darttut3.dart
@@ -5,18 +5,29 @@
 //Вариант 1 - ищем кол-во всех продуктов (iphone-15шт, банан-2шт и тд просто число)
 //Получаем список продуктов 
 
-import 'package:darttut3/darttut3.dart' as darttut3;
-import 'dart:convert';
+import 'dart:convert' as convert; //импортим конверты и http
 import 'package:http/http.dart' as http;
 
-void main(List<String> arguments) async {
+void main(List<String> arguments) async { //используем метод по подключение к сайту дамиджсон
   final url = Uri.https(
   'dummyjson.com',
   '/products',
   );
-  final response = await http.get(url);
+  final response = await http.get(url); //используем async await Для того чтобы дождать ответа от сайта
 
-  if(response.statusCode != 200){
+  if(response.statusCode != 200){ //условие того, что при ошибке будет выводиться error
     print('Error');
     return;
   }
+
+  Map<String,dynamic> jsonData = convert.jsonDecode(response.body); //Создаем map 
+  
+  List<dynamic> products = jsonData["products"]; //раскладываем массив продукты
+  int summa = 0;
+  
+  List<int> colvo = products.map((s) => s["stock"] as int).toList(); //ищем в массиве значение стонк
+  
+  summa = colvo.reduce((id, s) => id + s); //суммируем 
+  print("Сумма всех товаров состовляет: $summa"); //выводим
+}
+
